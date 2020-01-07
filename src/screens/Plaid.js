e999f6a53394e493e17a0e4717cfc637ebb1e45e
sarahcalvis <sarahcalvis@gmail.com>
2020-01-07 15:23:45 -0500

    kinda sorta started working on plaid but it is very hard

--- src/screens/Plaid.js
+++ src/screens/Plaid.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import $ from 'jquery';
 import { withStyles } from '@material-ui/styles';
 
 const styles = theme => ({
@@ -18,7 +21,51 @@ const styles = theme => ({
   },
 });
 
-class Home extends React.Component {
+class Plaid extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  
+  connectWithPlaid() {
+    var products = '<%= PLAID_PRODUCTS %>'.split(',');
+    var handler = Plaid.create({
+      apiVersion: 'v2',
+      clientName: 'Plaid Quickstart',
+      env: '<%= PLAID_ENV %>',
+      product: products,
+      key: '<%= PLAID_PUBLIC_KEY %>',
+      countryCodes: '<%= PLAID_COUNTRY_CODES %>'.split(','),
+      // webhook: 'https://your-domain.tld/plaid-webhook',
+      onSuccess: function(public_token) {
+        $.post('/get_access_token', {
+          public_token: public_token
+        }, function(data) {
+          $('#container').fadeOut('fast', function() {
+            $('#item_id').text(data.item_id);
+            $('#access_token').text(data.access_token);
+            $('#intro').hide();
+            $('#app, #steps').fadeIn('slow');
+          });
+        });
+      },
+    });
+    var accessToken = this.access_token;
+    if (accessToken != null && accessToken != '') {
+      $.post('/set_access_token', {
+        access_token: accessToken
+      }, function(data) {
+        $('#container').fadeOut('fast', function() {
+          $('#item_id').text(data.item_id);
+          $('#access_token').text(accessToken);
+          $('#intro').hide();
+          $('#app, #steps').fadeIn('slow');
+        });
+      });
+    }
+  }
   render() {
     const { classes } = this.props;
     return (
@@ -37,12 +84,29 @@ class Home extends React.Component {
             align="center"
             color="textSecondary"
             paragraph>
-            Work in progress.
+            Click the button below to open a list of Institutions. After you select one, you’ll be guided through an authentication process. Upon completion, a public_token will be passed back to the server and exchanged for access_token.
           </Typography>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+          <Button 
+            id="link-btn" 
+            className={classes.heroButton}
+            color="primary"
+            variant="contained"
+            size="large"
+            align="center"
+            onClick="this.connectWithPlaid">
+            Connect with Plaid
+          </Button>
+          </Grid>
         </Container>
       </div>
     );
   }
 }
 
-export default withStyles(styles)(Home);
+export default withStyles(styles)(Plaid);
