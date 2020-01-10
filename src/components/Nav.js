b092bffc358aa4b6b63b761793b3ad45f0123f9c
sarahcalvis <sarahcalvis@gmail.com>
2020-01-10 12:52:11 -0500

    working on fixing console errors- broken

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/styles';
 import { AppBar, Tabs, Tab } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   root: {
     margin: theme.spacing(0, 0, 0),
   },
@@ -12,20 +12,30 @@ const useStyles = makeStyles(theme => ({
     flexWrap: 'wrap',
 
   },
-}));
+});
+class Nav extends Component {
+  state = {
+    selectedTab: 'shop-setup'
+  }
 
-export default function NavBar() {
-  const classes = useStyles();
-  return (
-    <React.Fragment>
-      <AppBar className={classes.appBar}>
-        <Tabs >
-          <Tab label="Home" to='/' component={Link} />
-          <Tab label="PayPal Donate Button" to='/paypal-donate-button' component={Link} />
-          <Tab label="Stripe" to='/stripe' component={Link} />
-          <Tab label="Plaid" to='/plaid' component={Link} />
-        </Tabs>
-      </AppBar>
-    </React.Fragment>
-  );
+  handleTabClick = (event, value) => {
+    this.setState({ selectedTab: value });
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <React.Fragment>
+        <AppBar className={classes.appBar}>
+          <Tabs >
+            <Tab label="Home" to='/' component={Link} />
+            <Tab label="PayPal Donate Button" to='/paypal-donate-button' component={Link} />
+            <Tab label="Stripe" to='/stripe' component={Link} />
+            <Tab label="Plaid" to='/plaid' component={Link} />
+          </Tabs>
+        </AppBar>
+      </React.Fragment>
+    );
+  }
 }
+
+export default withStyles(styles)(Nav);
