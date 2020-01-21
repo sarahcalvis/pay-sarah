c5472578a5e9deaae7907ef949b383b73068d60c
sarahcalvis <sarahcalvis@gmail.com>
2020-01-21 11:58:59 -0500

    Converted App.js to a function

--- src/App.js
+++ src/App.js
@@ -8,25 +8,21 @@ import { ThemeProvider, CssBaseline } from '@material-ui/core';
 import theme from './theme';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 
-class App extends React.Component {
-  render() {
-    return (
-      <StripeProvider apiKey="pk_test_y69Z0N4wM6r6dyy6Sh4kcrWH00bivSnSRM">
-        <Elements>
-          <div>
-            <BrowserRouter>
-              <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <Nav />
-                <Routes />
-                <Footer position='fixed' left='0' bottom='0' />
-              </ThemeProvider>
-            </BrowserRouter>
-          </div>
-        </Elements>
-      </StripeProvider >
-    );
-  }
+export default function App() {
+  return (
+    <StripeProvider apiKey="pk_test_y69Z0N4wM6r6dyy6Sh4kcrWH00bivSnSRM">
+      <Elements>
+        <div>
+          <BrowserRouter>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <Nav />
+              <Routes />
+              <Footer position='fixed' left='0' bottom='0' />
+            </ThemeProvider>
+          </BrowserRouter>
+        </div>
+      </Elements>
+    </StripeProvider >
+  );
 }
-
-export default App;
