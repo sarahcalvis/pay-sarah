5b64744110f4f3e037ac5acef42e568fe8c7caaa
sarahcalvis <sarahcalvis@gmail.com>
2020-01-21 11:55:12 -0500

    Converted Stripe class to a function!

--- src/screens/Stripe.js
+++ src/screens/Stripe.js
@@ -5,7 +5,7 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
-import { withStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/styles';
 import Text from '../components/Text.js';
 import Snack from '../components/Snack.js';
 import {
@@ -15,21 +15,19 @@ import {
 import Button from '@material-ui/core/Button';
 import styles from '../styles.js';
 
-class Stripe extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      complete: false,
-      amount: ''
-    };
-    this.submit = this.submit.bind(this);
-  }
+const useStyles = makeStyles(styles);
 
-  async submit(ev) {
-    let i = parseInt(this.state.amount) * 100;
+function Stripe(props) {
+  const classes = useStyles();
+
+  const [complete, setComplete] = React.useState(false);
+  const [amount, setAmount] = React.useState('');
+
+  async function submit(ev) {
+    let i = parseInt(amount) * 100;
     console.log(i);
     /// payment request
-    let { token } = await this.props.stripe.createToken({ name: "Giving Tree Donor" });
+    let { token } = await props.stripe.createToken({ name: "Giving Tree Donor" });
     let response = await fetch("/charge", {
       method: "POST",
       headers: { "Content-Type": "text/plain" },
@@ -37,53 +35,50 @@ class Stripe extends React.Component {
     });
 
     if (response.ok) { console.log(response) }
-    if (response.ok) this.setState({ complete: true });
+    if (response.ok) setComplete(true);
   }
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.heroContent}>
-        <Container maxWidth="sm">
-          <Text type="heading" text="Stripe" />
-          <Text type="subheading" text="Enter your payment information below to complete the purchase. Not working? Start the server 😉" />
-        </Container>
-        {this.state.complete ?
-          <Snack /> :
-          <React.Fragment>
-            <main className={classes.layout}>
-              <Paper elevation={3} className={classes.paper}>
-                <Text type="card-heading" text="Make a Donation" />
-                <Typography
-                  component="h1"
-                  color="textSecondary"
-                  variant="subtitle1">
-                  Refer to <Link color="secondary" href="https://stripe.com/docs/testing" > Stripe testing information</Link> to try this out
+  return (
+    <div className={classes.heroContent}>
+      <Container maxWidth="sm">
+        <Text type="heading" text="Stripe" />
+        <Text type="subheading" text="Enter your payment information below to complete the purchase. Not working? Start the server 😉" />
+      </Container>
+      {complete ?
+        <Snack /> :
+        <React.Fragment>
+          <main className={classes.layout}>
+            <Paper elevation={3} className={classes.paper}>
+              <Text type="card-heading" text="Make a Donation" />
+              <Typography
+                component="h1"
+                color="textSecondary"
+                variant="subtitle1">
+                Refer to <Link color="secondary" href="https://stripe.com/docs/testing" > Stripe testing information</Link> to try this out
                 </Typography>
+              <React.Fragment>
                 <React.Fragment>
-                  <React.Fragment>
-                    <CardElement className={classes.stripeElement} />
-                    <input
-                      className={classes.stripeElement}
-                      value={this.state.amount}
-                      placeholder="Amount"
-                      onInput={e => this.setState({ amount: e.target.value })} />
-                    <Button
-                      color="primary"
-                      className={classes.button}
-                      variant="contained"
-                      onClick={this.submit}>
-                      Donate
+                  <CardElement className={classes.stripeElement} />
+                  <input
+                    className={classes.stripeElement}
+                    value={amount}
+                    placeholder="Amount"
+                    onInput={e => setAmount(e.target.value)} />
+                  <Button
+                    color="primary"
+                    className={classes.button}
+                    variant="contained"
+                    onClick={submit}>
+                    Donate
                     </Button>
-                  </React.Fragment>
                 </React.Fragment>
-              </Paper>
-            </main>
-          </React.Fragment>
-        }
-      </div>
-    );
-  }
+              </React.Fragment>
+            </Paper>
+          </main>
+        </React.Fragment>
+      }
+    </div>
+  );
 }
 
-export default injectStripe(withStyles(styles)(Stripe));
+export default injectStripe(Stripe);
